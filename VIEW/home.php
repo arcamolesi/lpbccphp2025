34a9372d19025dbf1e0efcbc808a57241cc7082f
Almir Rogério Camolesi <camolesi@fema.edu.br>
2025-06-02 20:56:08 -0300

    Atualização com pasta de validação
    Atualização com pasta de validação

--- VIEW/home.php
+++ VIEW/home.php
@@ -6,6 +6,11 @@
 <html lang="en">
 
 <head>
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+ 
+ 
+    <script src="/lpbccphp2025/view/js/init.js"></script>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
